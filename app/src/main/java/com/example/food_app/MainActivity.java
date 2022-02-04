423a320014eda8a41138db4a33590c4bd3207bb2
VirendraYadav1234 <virendrayadavv59@gmail.com>
2022-02-04 10:47:05 +0530

    sin_in and sign_up page add

--- app/src/main/java/com/example/food_app/MainActivity.java
+++ app/src/main/java/com/example/food_app/MainActivity.java
@@ -2,17 +2,46 @@ package com.example.food_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+     Button login,logout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+      login=findViewById(R.id.btnlogin);
+      logout=findViewById(R.id.btnlogout);
+
+       login.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+
+        Toast.makeText(MainActivity.this, "Login has been clicked", Toast.LENGTH_LONG).show();
+
+        Intent intent=new Intent(MainActivity.this,activity_sign_in.class);
+        startActivity(intent);
+    }
+});
+         logout.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 Toast.makeText(MainActivity.this, "sign up", Toast.LENGTH_SHORT).show();
+                 Intent intent=new Intent(MainActivity.this,signUp_activity.class);
+                 startActivity(intent);
+             }
+         });
+
 
 
     }
+
+
+
 }
--- app/src/main/java/com/example/food_app/signUp_activity.java
+++ app/src/main/java/com/example/food_app/signUp_activity.java
@@ -2,13 +2,26 @@ package com.example.food_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class signUp_activity extends AppCompatActivity {
 
+Button logg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        logg=findViewById(R.id.button_up);
+        logg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(signUp_activity.this,activity_sign_in.class);
+                startActivity(intent);
+            }
+        });
     }
 }
